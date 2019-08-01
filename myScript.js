97bff4aa0c91d15676515993c65721b820b0d9a4
Ganesh Chowdhari <ganesh_chowdhari@persistent.com>
2019-08-01 16:27:45 +0530

    Updated html and js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,3 @@
 function myFunction() {
- document.getElementById("demo").innerHTML = "5.0.0 of us Paragraph changed.";
+ document.getElementById("demo").innerHTML = "6.0.0 of us Paragraph changed.";
 }
