00a1ba0c600e3bd3ecb27ebbdc64dfa438cd2809
Ganesh Chowdhari <ganesh_chowdhari@persistent.com>
2020-10-30 17:20:44 +0530

    Updated to 9.0.1

--- myScript.js
+++ myScript.js
@@ -1,3 +1,3 @@
 function myFunction() {
- document.getElementById("demo").innerHTML = "9.0.0 of us Paragraph changed.";
+ document.getElementById("demo").innerHTML = "9.1.0 of us Paragraph changed.";
 }
